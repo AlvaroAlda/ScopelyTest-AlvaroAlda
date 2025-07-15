6928bf97751d65ca175388439351c0f3cd653bda
AlvaroAlda <alvaro.caiman@gmail.com>
2025-07-15 07:56:45 +0200

    Creep logic

--- Assets/Scripts/Creep.cs
+++ Assets/Scripts/Creep.cs
@@ -1,13 +1,18 @@
+using System;
 using UnityEngine;
 
 public class Creep : MonoBehaviour, ITarget
 {
     [SerializeField] private CreepData creepData;
     [SerializeField] private CreepEvents creepEvents;
+
+    private bool _creepInitialized;
+    private float _currentLife;
+    private float _hitTime;
     
     private Vector3 _targetPosition;
-    private float _currentLife;
-
+    private DefendingBase _defendingBase;
+    
     public Vector3 TargetPosition => transform.position;
     
     // Start is called before the first frame update
@@ -15,7 +20,43 @@ public class Creep : MonoBehaviour, ITarget
     {
         _currentLife = creepData.MaxLife;
     }
-    
+
+    public void InitCreep(DefendingBase defendingBase, Vector3 spawningPoint)
+    {
+        _defendingBase = defendingBase;
+        
+        transform.position = spawningPoint;
+        transform.LookAt(defendingBase.transform);
+
+        _creepInitialized = true;
+    }
+
+    private void Update()
+    {
+        if (!_creepInitialized)
+            return;
+        
+        var distance = Vector3.Distance(_defendingBase.transform.position, transform.position);
+        if (distance <= creepData.HitRange)
+            EvaluateHitBase();
+
+        else
+            transform.position += transform.forward * (Time.deltaTime * creepData.TravelSpeed);
+    }
+
+    private void EvaluateHitBase()
+    {
+        if (_hitTime >= 1 / creepData.HitRate)
+        {
+            _defendingBase.HitBase(creepData.HitDamage);
+            _hitTime = 0;
+        }
+        else
+        {
+            _hitTime += Time.deltaTime;
+        }
+    }
+
     public void HitTarget(float damage)
     {
         _currentLife -= damage;
@@ -26,6 +67,7 @@ public class Creep : MonoBehaviour, ITarget
     public void DestroyTarget()
     {
         creepEvents.TriggerCreepDestroyed(creepData);
+        _creepInitialized = true;
         gameObject.SetActive(false);
     }
 }
